b742f7fb8dda3c8a9a24534a18662801fa619ffb
Noah Lake <nl21320@ms.wasatch.edu>
2022-04-07 21:32:55 -0600

    Completed Main Slate Function, Adding Swing Sliders
    Still need the scene and take JLabels to change through a JButton.

--- src/ProSlate.java
+++ src/ProSlate.java
@@ -1,12 +1,22 @@
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
+
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.MouseEvent;
+import java.io.File;
+
 import static java.lang.String.valueOf;
 
 public class ProSlate extends GraphicsProgram {
+
+    // creating global variables for slate
+    int currentScene = 1;
+    char currentShot = 'a';
+    int currentTake = 1;
+    JLabel take = new JLabel("Take " + currentTake);
+    JLabel scene = new JLabel("Scene " + currentScene + currentShot);
 
     // creating global variables for the navigation system
     public final String[] pages = new String[]{ "Slate","Documents" };
@@ -17,8 +27,10 @@ public class ProSlate extends GraphicsProgram {
     final double NAV_BTN_PADDING = 75;
     final double EST_BTN_HEIGHT = 32;
 
-    // instantiating the navigation JButtons
+    // instantiating the navigation and slate JButtons
     JButton slateButton = new JButton("Slate");
+    JButton docButton = new JButton("Documents");
+    JButton startSlateBtn = new JButton("Start");
 
     // instantiating the navigation bar
     GRect navBar = new GRect(0,0); // will be resized later
@@ -41,20 +53,76 @@ public class ProSlate extends GraphicsProgram {
         switch(ae.getActionCommand()){
             // navigation
             case "Slate":
-                print("Slate Button Pressed");
+                print("Slate button pressed");
                 currentPg = "Slate";
                 print("Page changed to: " + currentPg);
+                take.setVisible(true);
+                scene.setVisible(true);
+                startSlateBtn.setVisible(true);
                 break;
-
-            // settings
-            // TODO: ADD SETTINGS
+            case "Documents":
+                print("Documents button pressed");
+                currentPg = "Documents";
+                print("Page changed to: " + currentPg);
+                take.setVisible(false);
+                scene.setVisible(false);
+                startSlateBtn.setVisible(false);
+                break;
+            case "Start":
+                // * one of these method calls will output an error message due to not being on both computers at once.
+                playSound("/Users/NL21320/Documents/ProgrammingProjects/ProSlate/sounds/Slate Sound WAV.wav"); // macbook air pathname
+                playSound("insert pathname here"); // zephyrus 16 pathname
+                flashScreen();
+                break;
         }
     }
 
+    private void flashScreen(){
+        new Thread(new Runnable() {
+            public void run() {
+                GRect flash = new GRect(getWidth(), getHeight());
+                add(flash,0,0);
+                flash.setFilled(true);
+                flash.sendToFront();
+                flash.setVisible(true);
+                flash.setFillColor(new Color(238, 255, 0));
+                slateButton.setVisible(false);
+                docButton.setVisible(false);
+                startSlateBtn.setVisible(false);
+                pause(130);
+                flash.setFillColor(new Color(0, 140, 255));
+                pause(132);
+                flash.setVisible(false);
+                slateButton.setVisible(true);
+                docButton.setVisible(true);
+                startSlateBtn.setVisible(true);
+            }
+        }).start();
+    }
+
     private void createSlateSystem(){
-        JLabel scene = new JLabel("Scene");
-        add(scene, getWidth()/15,getHeight()/15);
-        scene.setFont(x);
+        // set up padding scalable ratio
+        int padding = 10;
+
+        // creating custom font
+        Font font1 = new Font("Sans Serif", Font.BOLD, 80);
+        Font font2 = new Font("Sans Serif", Font.PLAIN, 40);
+
+        // scene
+        add(scene, getWidth()/padding,getHeight()/padding);
+        scene.setFont(font1);
+        scene.setSize(400,80);
+
+        // take
+        add(take,getWidth()/padding,getHeight()/padding + 100);
+        take.setFont(font2);
+        take.setSize(400,50);
+
+        // adding the start button to the screen
+        add(startSlateBtn, navBar.getX()+navBar.getWidth() - (75 + 30),navBar.getY()-50);
+        print(startSlateBtn.getWidth());
+
+        addActionListeners();
     }
 
     private void createNavBar(){
@@ -69,27 +137,30 @@ public class ProSlate extends GraphicsProgram {
         // adding the slate navButton
         add(slateButton, NAV_BTN_PADDING*1,navBar.getY() + navBar.getHeight()/2 - EST_BTN_HEIGHT/2);
 
+        // adding the documents navButton
+        add(docButton, getWidth() - NAV_BTN_PADDING - 116,navBar.getY() + navBar.getHeight()/2 - EST_BTN_HEIGHT/2);
+        print(docButton.getWidth());
         // adding the action listeners to the buttons
         addActionListeners();
-
     }
 
-    private void positionNavIndication(){
-
-        // setting the navigation reference x position to whatever the page is
-        double buttonXPos = 0;
-        switch(currentPg){
-            case "Slate":
-                buttonXPos = slateButton.getX();
-                break;
-        }
-
-        // positioning the navIndication
+    private void positionNavIndication() {
         navIndication.setSize(navBar.getWidth()/3,navBar.getHeight());
         add(navIndication,navBar.getX(),navBar.getY());
         navIndication.setFilled(true);
         navIndication.setFillColor(new Color(88, 126, 133));
         navIndication.sendToFront();
+    }
+
+    public void playSound(String pathname){
+        File lol = new File(pathname);
+        try{
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(lol));
+            clip.start();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void print(String text){
